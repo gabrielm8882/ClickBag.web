862edc2290d53055d1e60772bc57a17a60d9ce62
Gabriel Martin Lopez <gabrielpunki2010@gmail.com>
2025-07-25 18:24:42 +0000

    Ok but you can also add some text after the get started button (main pag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -72,6 +72,9 @@ export default function Home() {
               </Button>
             </Link>
           </div>
+          <p className="mx-auto max-w-[700px] text-muted-foreground md:text-lg mt-6 italic">
+            What if you could make change without spending much time, laying a dime or moving a muscle?
+          </p>
         </div>
       </section>
 
